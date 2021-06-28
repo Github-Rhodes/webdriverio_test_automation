d6bd0ddd8c645a85f776438b3ed59a970b994ab5
Allen Rhodes <war2678@yahoo.com>
2021-06-28 08:13:03 -0500

    update

--- api-test/tests/api.test.js
+++ api-test/tests/api.test.js
@@ -1,67 +1,66 @@
-'use strict';
+"use strict";
 
 /* global supertest baseUrl, expect */
 
-describe('The Sauce Demo API', () => {
+describe("Api Automation Demo", () => {
   let postToPatch;
 
-  describe('renders Landing Page', () => {
-    it('GET / responds with 200 status and HTML.', () => {
+  describe("renders Landing Page", () => {
+    it("GET / responds with 200 status and HTML.", () => {
       return supertest(baseUrl)
-        .get('/')
+        .get("/")
         .expect(200)
-        .expect('Content-Type', /html/);
+        .expect("Content-Type", /html/);
     });
   });
 
-  describe('Posts-Related', () => {
-    const post = { userId: 1, title: 'New Title', body: 'New Body' };
+  describe("Posts-Related", () => {
+    const post = { userId: 1, title: "New Title", body: "New Body" };
 
-    it('GET /posts responds with 200 status and an array of post objects.', () => {
+    it("GET /posts responds with 200 status and an array of post objects.", () => {
       return supertest(baseUrl)
-        .get('/posts')
+        .get("/posts")
         .expect(200)
-        .expect(res => {          
-          expect(res.body).to.be.an('array');
-          expect(Object.keys(res.body[0])).to.deep.include(...Object.keys(post));
-          
-          postToPatch = res.body[0];  // GET & PATCH context
+        .expect((res) => {
+          expect(res.body).to.be.an("array");
+          expect(Object.keys(res.body[0])).to.deep.include(
+            ...Object.keys(post)
+          );
+
+          postToPatch = res.body[0]; // GET & PATCH context
         });
     });
 
-    it('POST /posts responds with 201 status and a post object.', () => {
+    it("POST /posts responds with 201 status and a post object.", () => {
       return supertest(baseUrl)
-        .post('/posts')
-        .set({ 'Content-type': 'application/json; charset=UTF-8' })
+        .post("/posts")
+        .set({ "Content-type": "application/json; charset=UTF-8" })
         .send(JSON.stringify(post))
         .expect(201)
-        .expect(res => {
-          expect(res.body).to.be.an('object');
+        .expect((res) => {
+          expect(res.body).to.be.an("object");
           expect(Object.keys(res.body)).to.deep.include(...Object.keys(post));
         });
     });
 
-    it('PATCH /posts responds with 200 status and the edited post object.', () => {
+    it("PATCH /posts responds with 200 status and the edited post object.", () => {
       const { id } = postToPatch;
 
       return supertest(baseUrl)
         .patch(`/posts/${id}`)
-        .set({ 'Content-type': 'application/json; charset=UTF-8' })
-        .send(post)  // GET & PATCH context
+        .set({ "Content-type": "application/json; charset=UTF-8" })
+        .send(post) // GET & PATCH context
         .expect(200)
-        .expect(res => {
+        .expect((res) => {
           delete res.body.id;
-          
-          expect(res.body).to.be.an('object');
+
+          expect(res.body).to.be.an("object");
           expect(res.body).to.eql(post);
         });
     });
 
-    it('DELETE /api/users/:userID responds with 200 status.', () => {
-      return supertest(baseUrl)
-        .delete('/posts/1')
-        .expect(200);
+    it("DELETE /api/users/:userID responds with 200 status.", () => {
+      return supertest(baseUrl).delete("/posts/1").expect(200);
     });
   });
 });
-
